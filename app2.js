b739ec751e0e57bc8f3c2337e4ef6336196510c4
Jordan Folk <jordan.folk7@gmail.com>
2021-09-02 21:52:36 -0700

    added number game

--- app2.js
+++ app2.js
@@ -22,3 +22,17 @@ function doyouwannagofast(){
         txt = "check yo self";
       }
     }
+    <script>
+    function guessGame(){
+        number = Math.floor(Math.random()*150);
+        document.write(number);
+        var guess = prompt("Guess a number: ");
+            do {
+                guess = prompt("Keep guessing!");
+                if (number < guess) {
+                    prompt("You're living in the clouds");
+                } else if (number > guess) {
+                    prompt("Get those numbas up");
+                } else document.write("Gurtz");
+                while (guess != number);
+    </script>
