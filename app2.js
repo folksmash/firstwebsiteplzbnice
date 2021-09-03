2630914daf2b851ab51e8490acfd49fa3934e94e
Jordan Folk <jordan.folk7@gmail.com>
2021-09-02 22:03:42 -0700

    updated prompts and loop

--- app2.js
+++ app2.js
@@ -1,38 +1,40 @@
-let userName = prompt('Please Enter your Name: ');
+function tellmename(){
+  let userName = prompt('TELL ME YOUR NAME:');
 
-if(userName == 'Jordan'){
-    document.write('Bananarama');
-} else {
-    document.write(userName)
+  if(userName == 'Jordan'){
+      document.write('Time to go fast ' + userName);
+  } else {
+      document.write(userName)    
+  }
 }
-function getUserName(){
-	let userName = prompt('Please Enter your Name: ');
+var userAnswer = prompt('Do you wanna zoom?');
 
-	if(userName == 'Jordan'){
-		document.write('Welcome ' + userName);
-	}else {
-		document.write('Welcome' + userName);
-	}
+function sleepfan(){
+  console.log(userAnswer);
+  if(userAnswer.toLowerCase() == 'no'){
+      let url = "https://fm.cnbc.com/applications/cnbc.com/resources/img/editorial/2012/11/09/100110389-drive-fast-gettyp.1910x1000.jpg?v=1424731074"
+      document.write('<img src="' + url + '">');
+  }
 }
-
-function doyouwannagofast(){
-    if (confirm("Press ok if you knew you wanted to go fast all along")) {
-        txt = "Of course you did";
-      } else {
-        txt = "check yo self";
+function gimmeyonumba(){
+  let correctAnswer = 200
+  let letsPlay = prompt('Caaan I have it? (yay or nay)');
+  console.log(userAnswer)
+  while(letsPlay.toLowerCase() == 'yay'){
+      let numberOfAttempts = 6;
+      for(let i = 1; i <= numberOfAttempts; i++){
+          userAnswer = prompt("What's the best number between 1-300");
+          if(userAnswer == correctAnswer){
+              alert('Try again' + (numberOfAttempts - i) + 'attempts left');
+              break;
+          } else if(userAnswer < correctAnswer){
+              alert('Are you even trying?' + (numberOfAttempts - i) + 'attempts left');
+          } else if(userAnswer > correctAnswer){
+              alert('Are you even trying?' + (numberOfAttempts - i) + 'attempts left');
+      
+          }
       }
-    }
-    <script>
-    function guessGame(){
-        number = Math.floor(Math.random()*150);
-        document.write(number);
-        var guess = prompt("Guess a number: ");
-            do {
-                guess = prompt("Keep guessing!");
-                if (number < guess) {
-                    prompt("You're living in the clouds");
-                } else if (number > guess) {
-                    prompt("Get those numbas up");
-                } else document.write("Gurtz");
-                while (guess != number);
-    </script>
+      letsPlay = prompt('Wanna Play a Game? (yay or nay)');
+  }
+  
+}
